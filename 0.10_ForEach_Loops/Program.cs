51d9f1a2d024c931065ab90218b011d560d726a2
Daniel Schroeder <danielschroederjr@gmail.com>
2017-03-23 15:09:21 -0400

    added ForLoops, ForEachLoops, ForLoopSimpleProgram

---
+++ 0.10_ForEach_Loops/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._10_ForEach_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[5];
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+            numbers[3] = 4;
+            numbers[4] = 5;
+
+                    //place in the array  0   1   2   3   4  5
+            int[] lottoNums = new int[] { 15, 4, 16, 32, 20, 7 };
+            string[] names = new string[] { "James", "Paul", "Daniel", "Levi" };
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(lottoNums[5]);
+            
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("===================");
+
+            foreach (int number in lottoNums)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
--- 0.10_ForLoops/Program.cs
+++ 0.10_ForLoops/Program.cs
@@ -13,29 +13,43 @@ namespace _0._10_ForLoops
 
             //lcv = 
 
-           /* for (int lcv = 0; lcv < 15; lcv++)
-            {
-                Console.WriteLine("Paul is a Joker");
-            
-            for (int i = 0; i < 20; i++)
-            {
-                //Console.WriteLine("Paul is a Joker");
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine("Even");
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }
-            } 
-            */
+            /* for (int lcv = 0; lcv < 15; lcv++)
+             {
+                 Console.WriteLine("Paul is a Joker");
 
+             for (int i = 0; i < 20; i++)
+             {
+                 //Console.WriteLine("Paul is a Joker");
+                 if (i % 2 == 0)
+                 {
+                     Console.WriteLine("Even");
+                 }
+                 else
+                 {
+                     Console.WriteLine(i);
+                 }
+             } 
+
+
+             for (int i = 0; i <= 10; i++)
+
+             {
+                 Console.WriteLine(i);
+             }
+             */
+             //adding 1 + 2 + 3 + 4 etc
+            int sum = 0;
             for (int i = 0; i <= 10; i++)
-            
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine(sum);
+
+            for (int i = 10; i > 0; i--)
             {
                 Console.WriteLine(i);
             }
+
 
 
             Console.ReadLine();
