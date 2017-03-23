6e6a109b2ce1e546d5b205548ea7e022151211ad
Daniel Schroeder <danielschroederjr@gmail.com>
2017-03-23 12:08:16 -0400

    added Constructors

---
+++ 0.09_Constructors/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._09_Constructors
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Level { get; set; }
+
+        public Student()
+        {
+
+        }
+
+
+    }
+}
