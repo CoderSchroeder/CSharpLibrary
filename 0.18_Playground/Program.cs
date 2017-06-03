682a523f78d8355462d53c2e980333c779399ce2
Daniel A Schroeder <danielschroederjr@gmail.com>
2017-06-03 12:07:38 -0400

    added playground to test methods etc,

---
+++ 0.18_Playground/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._18_Playground
+{
+    class Scope
+    {
+        private int x = 10;
+        private static int y = 20;
+
+        public static void Main()
+        {
+            int x = 100;
+            int y = 200;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(Scope.y);
+            Console.ReadLine();
+        }
+
+        //{
+        //    int count = 0;
+        //    int m = 10;
+        //    int n = 7;
+        //    while (m % n >= 0)
+        //    {
+        //        m = m + 1;
+        //        //Console.WriteLine("The current m number is " + m);
+
+        //        n = n + 2;
+        //        //Console.WriteLine("The current n number is " + n );
+
+        //        count++;
+        //        //Console.WriteLine("The while loop executed " + count + " time(s)");
+
+        //    }
+        //    Console.WriteLine("The while loop executed " + count + " time(s)");
+
+
+        //    //Console.WriteLine("The while loop executes " + count + " times");
+        //    Console.ReadLine();
+        //}
+    }
+}
---
+++ MatchingPairsGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MatchingPairsGame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MatchingPairsGame")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f894bc2b-7f84-4dbd-88a9-2cff2b8bb9ab")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
